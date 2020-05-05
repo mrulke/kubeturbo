13aa64fe2d646f2dc75b264120f168275e052c87
Meng Ding <dingmeng@gmail.com>
2020-05-05 16:58:00 -0400

    Update go module and go vendor

--- vendor/github.com/turbonomic/turbo-go-sdk/pkg/builder/entity_dto_builder.go
+++ vendor/github.com/turbonomic/turbo-go-sdk/pkg/builder/entity_dto_builder.go
@@ -98,7 +98,7 @@ type EntityDTOBuilder struct {
 	virtualDataCenterData  *proto.EntityDTO_VirtualDatacenterData
 	storageControllerData  *proto.EntityDTO_StorageControllerData
 	logicalPoolData        *proto.EntityDTO_LogicalPoolData
-	virtualApplicationData *proto.EntityDTO_VirtualApplicationData
+	serviceData            *proto.EntityDTO_ServiceData
 	containerPodData       *proto.EntityDTO_ContainerPodData
 	containerData          *proto.EntityDTO_ContainerData
 	workloadControllerData *proto.EntityDTO_WorkloadControllerData
@@ -163,8 +163,8 @@ func (eb *EntityDTOBuilder) Create() (*proto.EntityDTO, error) {
 		entityDTO.EntityData = &proto.EntityDTO_StorageControllerData_{eb.storageControllerData}
 	} else if eb.logicalPoolData != nil {
 		entityDTO.EntityData = &proto.EntityDTO_LogicalPoolData_{eb.logicalPoolData}
-	} else if eb.virtualApplicationData != nil {
-		entityDTO.EntityData = &proto.EntityDTO_VirtualApplicationData_{eb.virtualApplicationData}
+	} else if eb.serviceData != nil {
+		entityDTO.EntityData = &proto.EntityDTO_ServiceData_{eb.serviceData}
 	} else if eb.containerPodData != nil {
 		entityDTO.EntityData = &proto.EntityDTO_ContainerPodData_{eb.containerPodData}
 	} else if eb.containerData != nil {
@@ -421,16 +421,16 @@ func (eb *EntityDTOBuilder) ContainerData(containerData *proto.EntityDTO_Contain
 	return eb
 }
 
-func (eb *EntityDTOBuilder) VirtualApplicationData(vAppData *proto.EntityDTO_VirtualApplicationData) *EntityDTOBuilder {
+func (eb *EntityDTOBuilder) ServiceData(serviceData *proto.EntityDTO_ServiceData) *EntityDTOBuilder {
 	if eb.err != nil {
 		return eb
 	}
 	if eb.entityDataHasSet {
-		eb.err = fmt.Errorf("EntityData has already been set. Cannot use %v as entity data.", vAppData)
+		eb.err = fmt.Errorf("EntityData has already been set. Cannot use %v as entity data.", serviceData)
 
 		return eb
 	}
-	eb.virtualApplicationData = vAppData
+	eb.serviceData = serviceData
 	eb.entityDataHasSet = true
 	return eb
 }
